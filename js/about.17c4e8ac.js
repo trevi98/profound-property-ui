eaafed8b8c220ad963da394a45df07ec7d3970e7
abdullatef trefi <abdullatef.trefi98@gmail.com>
2022-11-15 13:00:28 +0400

    initial

---
+++ js/about.17c4e8ac.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkui"]=self["webpackChunkui"]||[]).push([[443],{7381:function(n,u,t){t.r(u),t.d(u,{default:function(){return l}});var a=t(3396);const e={class:"about"},r=(0,a._)("h1",null,"This is an about page",-1),s=[r];function i(n,u){return(0,a.wg)(),(0,a.iD)("div",e,s)}var c=t(89);const o={},f=(0,c.Z)(o,[["render",i]]);var l=f}}]);
+//# sourceMappingURL=about.17c4e8ac.js.map
